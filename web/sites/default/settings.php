f32962831885d44cb6b2007940545d6591974476
Federico Antón <federico@grupoite.com.uy>
2024-01-25 11:02:09 -0300

    Drupal 10

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -249,7 +249,7 @@ $settings['config_sync_directory'] = '../config';
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '0miUTdayPOplOFTVjl5kxi6P5k3b7qohtM3k9H8Vq-kcQZNHipTAJnSMNXHFDCMfdz0_za21nw';
+$settings['hash_salt'] = 'sj-3ABDQPHD91zrDizwWVj-6HLknxEeVfGCeiVojT0FpRbr88kClzJK2VQevNQJpusEQJ6pJuQ';
 
 /**
  * Deployment identifier.
@@ -793,3 +793,27 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+/*$databases['default']['default'] = array (
+  'database' => 'basicsite2',
+  'username' => 'root',
+  'password' => 'fede',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+*/
+$databases['default']['default'] = array (
+  'database' => 'basicsite',
+  'username' => 'root',
+  'password' => 'fede',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
